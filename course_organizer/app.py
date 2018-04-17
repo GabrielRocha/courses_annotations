16c5b0290996c120b11799d7d16e4cb05cf288c6
Gabriel Rocha <gabrielrocha@catho.com.br>
2018-04-16 22:33:44 -0300

    Using tinymce to text editor

--- course_organizer/app.py
+++ course_organizer/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from directory_inspect import get_files, find_directory
+from werkzeug.utils import secure_filename
 from settings import COURSE_PATH
 
-app = Flask(__name__, static_url_path="/static", static_folder=COURSE_PATH)
+app = Flask(__name__, static_url_path="/static")
 
 
 COUSE_STRUCTURE = get_files()
@@ -18,10 +19,14 @@ def index():
 def chapter(chapter_name):
     directory, files = find_directory(chapter_name, COUSE_STRUCTURE)
     context = {"folders": sorted(directory),
-               "files": sorted(files.items(), key=lambda x:x[0]),
+               "files": sorted(files.items(), key=lambda x:x[0][1]),
                "chapter": chapter_name}
     return render_template("chapter.html", **context)
 
+
+@app.route("/file/<path:filename>")
+def send_files(filename):
+    return send_from_directory(COURSE_PATH, filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
