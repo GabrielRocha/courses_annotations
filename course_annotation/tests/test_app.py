258ee030d745ffc9b3223139a7fdda96f032073f
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2018-06-14 21:33:33 -0300

    Test app get foders videos and annotation

--- course_annotation/tests/test_app.py
+++ course_annotation/tests/test_app.py
@@ -18,3 +18,38 @@ def app():
 def test_index(client, tag):
     response = client.get('/')
     assert tag in response.data.decode()
+
+
+@pytest.mark.parametrize('url, file, annotation', [
+    (
+        '/chapter/chapter_1/',
+        'link="chapter_1/example.html"',
+        'source src="/file/chapter_1/example.mp4"'
+    ),
+    (
+        '/chapter/chapter_2/',
+        '',
+        'source src="/file/chapter_2/example_2.mp4"'
+    ),
+    (
+        '/chapter/chapter_3/',
+        '',
+        'source src="/file/chapter_3/example_3.mp4"'
+    ),
+])
+def test_folder_videos_and_annotations(client, url, file, annotation):
+    response = client.get(url)
+    assert file in response.data.decode()
+    assert annotation in response.data.decode()
+
+
+def test_second_level_folder(client):
+    response = client.get('/chapter/chapter_2/')
+    tag = '<a href="/chapter/chapter_2/folder/">folder</a>'
+    assert tag in response.data.decode()
+
+
+def test_videos_in_second_level_folder(client):
+    response = client.get('/chapter/chapter_2/folder/')
+    video = 'source src="/file/chapter_2/folder/example_2_1.mp4"'
+    assert video in response.data.decode()
