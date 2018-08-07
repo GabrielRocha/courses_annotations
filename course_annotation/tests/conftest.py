fe2c4fd72aea02bc2ae9f0f5c61ad5a012242a35
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2018-08-07 12:46:16 -0300

    Refactor the test

---
+++ course_annotation/tests/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+
+
+@pytest.fixture
+def course_structure():
+    return {
+        'chapter_1': {
+            'folders': {},
+            'files': {
+                'example': {
+                    'video': 'example.mp4',
+                    'annotation': 'example.html'
+                }
+            }
+        },
+        'chapter_2': {
+            'folders': {
+                'folder': {
+                    'folders': {},
+                    'files': {
+                        'example_2_1': {
+                            'video': 'example_2_1.mp4'
+                        }
+                    }
+                }
+            },
+            'files': {
+                'example_2': {
+                    'video': 'example_2.mp4'}
+            }
+        },
+        'chapter_3': {
+            'folders': {},
+            'files': {
+                'example_3': {
+                    'video': 'example_3.mp4'
+                }
+            }
+        }
+    }
